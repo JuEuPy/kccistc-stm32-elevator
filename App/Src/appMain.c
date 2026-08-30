95d7736afce72aecdd06af0d699fe905b164f266
hyeon yu <yhsys0429@gmail.com>
2026-08-30 16:11:58 +0900

    uart 수정

--- App/Src/appMain.c
+++ App/Src/appMain.c
@@ -15,7 +15,7 @@
 
 // Teleplot 통신용
 int __io_putchar(int ch){
-    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
     return ch;
 }
 
